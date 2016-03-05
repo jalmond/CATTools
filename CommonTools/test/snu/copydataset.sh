102fd9181e318d62c38d02e46e79e4398edfe42d
jalmond <jalmond@cern.ch>
2016-03-05 23:16:44 +0900

    a

---
+++ CommonTools/test/snu/copydataset.sh
@@ -0,0 +1,2 @@
+ssh jalmond@cms3.snu.ac.kr rm -r /data1/LQAnalyzer_rootfiles_for_analysis/CATAnalysis/dataset/
+scp -r dataset jalmond@cms3.snu.ac.kr:/data1/LQAnalyzer_rootfiles_for_analysis/CATAnalysis/dataset_$CATVERSION
